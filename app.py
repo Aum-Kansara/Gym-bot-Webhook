ff1f10b08deda449520304763687ecbd6e9781d3
Aum Kansara <aumkan23@gmail.com>
2023-09-02 14:37:03 +0530

    added review and complaint summarizer routes

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask,jsonify,request
 import requests
 load_dotenv()
 webhook_link=os.getenv("WEBHOOK_URL")
+flowise_review=os.getenv("FLOWIAW_REVIEW_URL")
+flowise_complaint=os.getenv("FLOWIAW_COMPLAINT_URL")
 
 app=Flask(__name__)
 
@@ -27,6 +29,18 @@ def getResponse():
                         "booking_url":res.headers['bookingurl']
             })
     return jsonify({"question" : email})
+
+@app.route('/getComplaintSummary')
+def getComplaintSummary():
+    summary=request.args.get('complaint','')
+    res=requests.post(flowise_complaint,json={"question":summary})
+    return jsonify({"summary":res.text})
+
+@app.route('/getReviewSummary')
+def getReviewSummary():
+    summary=request.args.get('review','')
+    res=requests.post(flowise_review,json={"question":summary})
+    return jsonify({"summary":res.text})
 
 if __name__=="__main__":
     app.run(debug=True)
